af714e55db7523573404bd3ef14ce8dec4f76757
Gustavo_Félix <gustavofelix2007@gmail.com>
2025-11-01 18:46:07 -0300

    feat: implement PostgreSQL integration and security filter chain configuration

--- src/main/java/com/gustavo/springsecurity/repository/RoleRepository.java
+++ src/main/java/com/gustavo/springsecurity/repository/RoleRepository.java
@@ -1,7 +1,9 @@
-package com.gustavo.springsecurity.repositories;
+package com.gustavo.springsecurity.repository;
 
 import com.gustavo.springsecurity.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/com/gustavo/springsecurity/repository/TweetRepository.java
+++ src/main/java/com/gustavo/springsecurity/repository/TweetRepository.java
@@ -1,7 +1,9 @@
-package com.gustavo.springsecurity.repositories;
+package com.gustavo.springsecurity.repository;
 
 import com.gustavo.springsecurity.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
